5caa29c0a1bd4e6eaefbe4f502dbe418d131844d
joaojuvencio87 <joaojuvencio87@gmail.com>
2021-04-12 17:27:20 -0300

    Ajuste 1

---
+++ WebApi/Controllers/AddCategoriaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebPage.Models;
+
+namespace WebPage.Controllers
+{
+    public class AddCategoriaController : Controller
+    {
+        public IActionResult AdicionarCategoria(Categoria categoria)
+        {
+            return View(categoria);
+        }
+    }
+}
